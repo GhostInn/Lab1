b2f5aa6ff2fcd43815629931dbdda798966b532b
Vladislav Pronin <matyuninvladislav1@gmail.com>
2024-03-23 23:43:11 +0300

    Крайняя версия, возможно конечная

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,44 +1,37 @@
 ﻿using Lab1;
 public class Program
 {
-    static void Main(string[] args)
+    static void Main(string[] args)//В методе Main написал список объектов класса Aircraft и его производных классов FighterJet,
+                                   //Helicopter, Bomber, Convertiplane и Quadcopter. В искомые списки verticalTakeoffAircrafts и horizontalTakeoffAircrafts.
     {
-        List<Aircraft> aircrafts = new List<Aircraft>
+        List<Aircraft> verticalTakeoffAircrafts = new List<Aircraft>
             {
-                new FighterJet("Су-27", 1, 1, 1, 1),
                 new Helicopter("Ми-24", 1, 1, 1, 1),
-                new Bomber("Ту-4", 1, 1, 1, 1),
                 new Convertiplane("Ми-30", 1, 1, 1, 1),
                 new Quadcopter("Квадрокоптер", 1, 1, "Тип", "Связь")
             };
 
-        List<Aircraft> verticalTakeoffAircrafts = new List<Aircraft>();
-        List<Aircraft> horizontalTakeoffAircrafts = new List<Aircraft>();
+        List<Aircraft> horizontalTakeoffAircrafts = new List<Aircraft>
+            {
+                new FighterJet("Су-27", 1, 1, 1, 1),
+                new Bomber("Ту-4", 1, 1, 1, 1),
+            };
 
-        foreach (var aircraft in aircrafts)
-        {
-            if (aircraft is Helicopter || aircraft is Convertiplane || aircraft is Quadcopter)
-            {
-                verticalTakeoffAircrafts.Add(aircraft);
-            }
-            else
-            {
-                horizontalTakeoffAircrafts.Add(aircraft);
-            }
-        }
+        //Потом выводятся названия всех объектов в списке verticalTakeoffAircrafts с помощью оператора foreach и строки $"- {aircraft.Name}".
         Console.WriteLine("Системы с вертикальным взлётом:");
         foreach (var aircraft in verticalTakeoffAircrafts)
         {
-            Console.WriteLine($"- {aircraft.Name}");
+            Console.WriteLine($"{aircraft.Name}");
         }
 
+        //Также со списком horizontalTakeoffAircrafts.
         Console.WriteLine("\nСистемы с горизонтальным взлетом:");
         foreach (var aircraft in horizontalTakeoffAircrafts)
         {
-            Console.WriteLine($"- {aircraft.Name}");
+            Console.WriteLine($"{aircraft.Name}");
         }
     }
 }
-//Эта программа определяет базовый класс `Aircraft` и производные классы `FighterJet`, `Helicopter`, `Bomber`, `Convertiplane` и `Quadcopter`. Каждый класс имеет соответствующие свойства, определенные в вашем вопросе.
-//В методе `Main` создается список самолетов, который затем делится на два списка: `verticalTakeoffAircrafts` и `horizontalTakeoffAircrafts`. Затем программа выводит названия самолетов, относящихся к каждой группе.
+//В этой программе я определил базовый класс `Aircraft` и производные классы `FighterJet`, `Helicopter`, `Bomber`, `Convertiplane` и `Quadcopter`. Каждый класс имеет соответствующие свойства
+//В методе `Main` создается два списка самолетов: `verticalTakeoffAircrafts` и `horizontalTakeoffAircrafts`. Затем программа выводит названия самолетов, относящихся к каждой системе взлета.
 
