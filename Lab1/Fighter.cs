e21f8850e786e3965ce21fec0f3aef250b5c5315
moskva <moskva@8DA0>
2024-03-23 18:42:04 +0300

    Добавьте файлы проекта.

---
+++ Lab1/Fighter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public class Fighter
+    {
+        public void Maximum_Horizontal_Speed() { }
+        public void Maximum_Flight_Altitude() { }
+        public void Wingspan() { }
+        public void Afterburner_Speed() { }
+    }
+
+    public class Helicopter
+    {
+        public void Takeoff_Speed() { }
+        public void Maximum_Flight_Altitude() { }
+        public void Number_Of_Screws() { }
+        public void Maximum_Horizontal_Speed() { }
+    }
+
+    public class Bomber
+    {
+        public void Maximum_Horizontal_Speed() { }
+        public void Maximum_Flight_Altitude() { }
+        public void Maximum_Payload() { }
+        public void Wingspan() { }
+    }
+
+    public class Tiltrotor
+    {
+        public void Maximum_Horizontal_Speed() { }
+        public void Vertical_Flight_Speed() { }
+        public void Maximum_Flight_Altitude() { }
+        public void Wingspan() { }
+    }
+
+    public class Quadcopter_UAV
+    {
+        public void Maximum_Horizontal_Speed() { }
+        public void Maximum_Flight_Altitude() { }
+        public void Type() { }
+        public void Management_Protocol() { }
+    }
+}
